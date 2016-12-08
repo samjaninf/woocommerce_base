a246cad749fcb2361df487d7f09b7f6422972544
Jazzy Jones <narutoksan@gmail.com>
2016-12-08 15:15:43 -0500

    second push

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'c1pondhockey');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'c1pondhockey');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'dStrx2pQRK#');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
